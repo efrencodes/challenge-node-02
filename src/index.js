77681497f1c29df87ed5f7dde49bb99fc67ef0b6
efren <efren.martinez@marciano.com.mx>
2020-05-29 11:17:38 -0500

    [FEATURE]Web scrapping al titulo de atomix.vg

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
+/* eslint-disable no-console */
+/* eslint-disable implicit-arrow-linebreak */
 const puppeteer = require('puppeteer');
 
 const getDataFromPuppeteer = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://platzi.com');
-    await page.screenshot({
-      path: 'src/images/image.png'
-    });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf'
-    });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
+    await page.goto('https://atomix.vg/');
+    const titles = await page.evaluate(
+      () =>
+        document.getElementsByClassName('featured-image-narrow-title')[0].children[0].innerText,
     );
-    console.log(platziCourseTitle);
+    console.log(titles);
     console.log('Puppeteer End');
     await browser.close();
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 getDataFromPuppeteer();
